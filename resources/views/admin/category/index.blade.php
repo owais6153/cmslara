e480eccec90bf2c8bee8ecf17ef959ec3bdae8d7
owais6153 <owais6153@gmail.com>
2022-01-25 01:47:25 +0500

    Cat permissions

--- resources/views/admin/category/index.blade.php
+++ resources/views/admin/category/index.blade.php
@@ -75,6 +75,7 @@
 			    	</form>
 		    	</div>
          	@endcan
+         	@can('viewCategories')
          	<div class="col-md-8">
          		<div class="card shadow mb-4">
 		            <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
@@ -107,6 +108,7 @@
 		            </div>
 		        </div>
          	</div>
+         	@endcan
          </div>
 
     </div>
